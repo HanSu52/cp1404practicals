27bd0df1a2f12088eb42849ea0f9ebf067b1cebd
HanSu52 <han.su@my.jcu.edu.au>
2020-04-03 19:24:56 +0800

    Add password_check.py

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -3,54 +3,25 @@ Author: Han Su
 Date: 03/04/2020
 https://github.com/HanSu52/cp1404practicals
 """
-
-MIN_LENGTH = 2
-MAX_LENGTH = 6
-SPECIAL_CHARS_REQUIRED = False
-SPECIAL_CHARACTERS = "!@#$%^&*()_-=+`~,./'[]<>?{}|\\"
+MINIMUM_LENGTH = 6
 
 
 def main():
     """A program that asks the user for a password and prints asterisks based on its length."""
-    print("Please enter a valid password")
-    print("Your password must be between", MIN_LENGTH, "and", MAX_LENGTH,
-          "characters, and contain:")
-    print("\t1 or more uppercase characters")
-    print("\t1 or more lowercase characters")
-    print("\t1 or more numbers")
-    if SPECIAL_CHARS_REQUIRED:
-        print("\tand 1 or more special characters: ", SPECIAL_CHARACTERS)
-    password = input("> ")
-    while not is_valid_password(password):
-        print("Invalid password!")
-        password = input("> ")
-    print("Your {}-character password is valid: {}".format(len(password), password))
+    password = get_password(MINIMUM_LENGTH)
+    get_asterisks(password)
 
 
-def is_valid_password(password):
-    """Determine if the provided password is valid."""
-    if len(password) > MAX_LENGTH or len(password) < MIN_LENGTH:
-        return False
-    count_lower = 0
-    count_upper = 0
-    count_digit = 0
-    count_special = 0
-    for char in password:
-        if char.islower():
-            count_lower += 1
-        elif char.isupper():
-            count_upper += 1
-        elif char.isdigit():
-            count_digit += 1
-        elif char in SPECIAL_CHARACTERS:
-            count_special += 1
+def get_password(minimum_length):
+    password = input("Please enter a valid password with at least {} characters: ".format(minimum_length))
+    while len(password) < MINIMUM_LENGTH:
+        print("Your password is invalid.")
+        password = input("Please enter a valid password with at least 6 characters: ".format(minimum_length))
+    return password
 
-    if count_digit == 0 or count_upper == 0 or count_lower == 0:
-        return False
-    if SPECIAL_CHARS_REQUIRED:
-        if count_special == 0:
-            return False
-    return True
+
+def get_asterisks(password):
+    print(len(password) * "*")
 
 
 if __name__ == '__main__':
